02cfcd83e40a1155d35a1e0475ba45e5e854830f
mihailzarin <mihail.zarin@gmail.com>
2025-08-20 01:16:52 +0300

    fix: Add missing <a> tag manually

--- components/SpotifyConnect.tsx
+++ components/SpotifyConnect.tsx
@@ -2,9 +2,9 @@
 
 export default function SpotifyConnect() {
   return (
-    
-      href=\"/api/spotify/auth\"
-      className=\"block w-full bg-green-500 hover:bg-green-400 text-black font-bold py-3 px-4 rounded-lg transition text-center\"
+    <a
+      href="/api/spotify/auth"
+      className="block w-full bg-green-500 hover:bg-green-400 text-black font-bold py-3 px-4 rounded-lg transition text-center"
     >
       Connect Spotify
     </a>
